64a7a10a01bacbe5297b024f8f569b85066ad44c
Vatti Praveen <vattipraveen565@gmail.com>
2022-08-16 11:04:34 +0530

    Making cganges to hardhat-NFT

--- Ethereum/hardhat-NFT/scripts/transfer.js
+++ Ethereum/hardhat-NFT/scripts/transfer.js
@@ -1,3 +1,4 @@
+const { expect } = require("chai");
 const hre = require("hardhat");
 
 async function main() {
@@ -5,10 +6,16 @@ async function main() {
     const token = await Token.attach("0x5FbDB2315678afecb367f032d93F642f64180aa3")
     
     const myadd = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
-    const mynft = await token.mintRINFT(myadd, "1st NFT")
+    const reciept = await token.mintRINFT(myadd, "1st NFT")
     
-    console.log(mynft.data)
-    //console.log(await token.ownerOf(mynft))
+    //const reciept = await tx.wait();
+        
+    const event = reciept.events.find((event) => event.event === "Transfer");
+
+    console.log(event);
+    //const [from, to, nftId] = event.args;
+    //console.log(await token.ownerOf(nftId))
+
 
 }
 
