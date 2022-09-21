0f430a1bbeaadaca6197e0b7e457c45fac1ab5be
Vatti Praveen <vattipraveen565@gmail.com>
2022-09-21 11:44:46 +0530

    TimeLock contract

---
+++ Ethereum/TimeLock/TimeLock.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: UNLICENCED
+pragma solidity ^0.8.7;
+
+contract TimeLock {
+    address owner;
+    mapping(address => uint) lockedTime;
+    mapping(address => uint) stakeAmount;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier lock() {
+        require(
+            block.timestamp > lockedTime[msg.sender],
+            "Wait till the time deadline is over"
+        );
+        _;
+    }
+
+    function currentBlockTime() public view returns (uint time) {
+        time = block.timestamp;
+    }
+
+    function stakeAndLock(uint _time) public payable {
+        require(
+            block.timestamp < _time,
+            "You need to enter higher timestamp than current block timstamp"
+        );
+        require(msg.value > 0, "Amount should be more than zero");
+        stakeAmount[msg.sender] = msg.value;
+        lockedTime[msg.sender] = _time;
+    }
+
+    function getReward() public lock {
+        require(lockedTime[msg.sender] != 0, "first you have to add stake");
+        uint reward = (stakeAmount[msg.sender] / 100);
+        payable(msg.sender).transfer(reward);
+    }
+}
