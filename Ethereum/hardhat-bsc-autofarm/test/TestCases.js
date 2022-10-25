f2550e6d4c37232d90e4851855be245dcdd3a065
Vatti Praveen <vattipraveen565@gmail.com>
2022-10-25 11:57:22 +0530

    Testing AutoFarm through hardhat-BSC-fork.

--- Ethereum/hardhat-bsc-autofarm/test/TestCases.js
+++ Ethereum/hardhat-bsc-autofarm/test/TestCases.js
@@ -3,11 +3,11 @@ const { ethers } = require("hardhat");
 const helpers = require("@nomicfoundation/hardhat-network-helpers");
 
 describe("Uniswap test contract", function () {
-  var owner, addr1, addr2;
+  var owner, addr1, addr2, impersonatedSigner;
         
-  var Autofarm, hardhatAutofarm;
+  var Autofarm, hardhatAutofarm, autoFarmAddress, autoFarmContract;
 
-  it("1) Basic interaction with AutoFarm", async function () {
+  it("1) Impersonating as AutoFarm owner and set allocPoint of a pool", async function () {
 
     [owner, addr1, addr2] = await ethers.getSigners();
     Autofarm = await ethers.getContractFactory("Farm");
@@ -15,33 +15,59 @@ describe("Uniswap test contract", function () {
 
     //var autoFarmAddress = "0x0895196562C7868C5Be92459FaE7f877ED450452";
 
-    const autoFarmAddress = "0x0895196562C7868C5Be92459FaE7f877ED450452";
-    const autoFarmContract = await hre.ethers.getContractAt("AutoFarmV2", autoFarmAddress);
+    autoFarmAddress = "0x0895196562C7868C5Be92459FaE7f877ED450452";
+    autoFarmContract = await hre.ethers.getContractAt("AutoFarmV2", autoFarmAddress);
 
 
     
     var autoOwner = await hardhatAutofarm.getOwner();
 
     await helpers.impersonateAccount(autoOwner);
-    const impersonatedSigner = await ethers.getSigner(autoOwner);
+    impersonatedSigner = await ethers.getSigner(autoOwner);
 
-    console.log(impersonatedSigner);
+    //console.log(impersonatedSigner);
 
     var allocValue = await autoFarmContract.poolInfo(56);
 
-    console.log(allocValue);
+    //console.log(allocValue);
     var pid = 56;
     var allocpoint = 1000;
     var booleanVal = false;
     var tx = await autoFarmContract.connect(impersonatedSigner).set(pid, allocpoint, booleanVal);
     const reciept2 = await tx.wait();
 
-    allocValue = await autoFarmContract.poolInfo(56);
+    poolInfo = await autoFarmContract.poolInfo(56);
 
-    console.log(allocValue);
+    //console.log(poolInfo.allocPoint);
 
 
-    //expect(await hardhatAutofarm).to.equal(liquidityValue);
+    expect(poolInfo.allocPoint).to.equal(allocpoint);
   });
+
+  it("2) Transfering ownership to addr1.", async function () {
+
+    var tx = await autoFarmContract.connect(impersonatedSigner).transferOwnership(addr1.address);
+    const reciept2 = await tx.wait();
+
+    //console.log(poolInfo.allocPoint);
+
+
+    expect(await hardhatAutofarm.getOwner()).to.equal(addr1.address);
+  });
+
+  it("3) Checking total alloc points.", async function () {
+
+    var totalAlloc = await autoFarmContract.totalAllocPoint();
+
+    //console.log(totalAlloc);
+    var pid = 56;
+    var allocpoint = 0; // setting allocPoint of pool-56 to zero from 1000
+    var booleanVal = false;
+    var tx = await autoFarmContract.connect(impersonatedSigner).set(pid, allocpoint, booleanVal);
+    const reciept2 = await tx.wait();
+
+    expect(await autoFarmContract.totalAllocPoint()).to.equal((totalAlloc - 1000));
+  });
+
 
 });
